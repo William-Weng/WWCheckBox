9fa3cf78e1bf56a311df27834217227a867b221e
William.Weng <William.Weng@iit.com.hk>
2023-03-01 17:15:46 +0800

    - 上傳Example

--- Example/Example/ViewController/ViewController.swift
+++ Example/Example/ViewController/ViewController.swift
@@ -1,23 +1,11 @@
-# WWCheckBox
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by William.Weng on 2022/12/15.
+//  ~/Library/Caches/org.swift.swiftpm/
+//  file:///Users/william/Desktop/@WWCheckBox
 
-[![Swift-5.7](https://img.shields.io/badge/Swift-5.7-orange.svg?style=flat)](https://developer.apple.com/swift/) [![iOS-13.0](https://img.shields.io/badge/iOS-13.0-pink.svg?style=flat)](https://developer.apple.com/swift/) ![](https://img.shields.io/github/v/tag/William-Weng/WWCheckBox) [![Swift Package Manager-SUCCESS](https://img.shields.io/badge/Swift_Package_Manager-SUCCESS-blue.svg?style=flat)](https://developer.apple.com/swift/) [![LICENSE](https://img.shields.io/badge/LICENSE-MIT-yellow.svg?style=flat)](https://developer.apple.com/swift/)
-
-HTML-like CheckBox function.
-仿HTML的CheckBox功能.
-
-![](./Example.gif)
-
-### [Installation with Swift Package Manager](https://medium.com/彼得潘的-swift-ios-app-開發問題解答集/使用-spm-安裝第三方套件-xcode-11-新功能-2c4ffcf85b4b)
-```
-dependencies: [
-    .package(url: "https://github.com/William-Weng/WWCheckBox.git", .upToNextMajor(from: "1.0.0"))
-]
-```
-
-### Example
-![](./IBDesignable.png)
-
-```swift
 import UIKit
 import WWPrint
 import WWCheckBox
@@ -63,4 +51,3 @@ private extension ViewController {
         myCheckBox3.indexPath = IndexPath(row: 3, section: 0)
     }
 }
-```
